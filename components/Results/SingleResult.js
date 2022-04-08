86c3327d5c81a0a3de9af3a1c9a3116afa785ea9
david mola <daviemola@gmail.com>
2022-04-09 03:07:15 +0530

    testing with huge data

--- components/Results/SingleResult.js
+++ components/Results/SingleResult.js
@@ -3,44 +3,95 @@ import Image from "next/image";
 
 export default function SingleResult({ show }) {
   return (
-    <div className="single-result wow fadeInUp delay-0-2s" key={show.id}>
+    <div className="single-result wow fadeInUp delay-0-6s">
       <div className="row align-items-center">
         <div className="col-xl-6 pr-0">
           <div className="results-img">
-            <div>
-              <img src={show.image} alt="Image" style={{ height: "501px" }} />
-            </div>
-            <span>{show.time}</span>
+            <a href="#">
+              <img
+                src={`${show.external_image_link}`}
+                alt="Image"
+                style={{ height: "501px" }}
+              />
+            </a>
+            <span>Thursday 9:30 pm</span>
           </div>
         </div>
         <div className="col-xl-6 pl-0">
           <div className="results-content">
             <span>
-              <i className="fa fa-calendar-o"></i> NOV 25, 2021
+              <i className="fa fa-calendar-o"> </i> NOV 25, 2021{" "}
             </span>
             <h3>
-              <a href="#">{show.name}</a>
+              <a href="#">{show.show_title}</a>
             </h3>
-            <p>{`${show.city} · ${show.venue}`}</p>
-            <p>{show.desc}</p>
+            <p>New York, NY · Manhattan Center</p>
+            <p>
+              Amet nulla facilisi morbi tempus iaculis urna id volutpat.
+              Placerat orci nulla pellentesque dignissim enim sit amet venenatis
+              urna. Mi proin sed libero enim sed. Aliquam malesuada bibendum
+              arcu vitae.
+            </p>
             <ul>
-              {show.performers.map((show, index) => (
-                <li key={index}>
-                  <a href="#">
-                    <div className="result-img">
-                      <img src={show.image} alt="Image" />
-                    </div>
-                    <span>
-                      {show.name.split(" ")[0]}
-                      <br />
-                      {show.name.split(" ")[1]}
-                    </span>
-                  </a>
-                </li>
-              ))}
+              <li>
+                <a href="#">
+                  <div className="result-img">
+                    <img src="assets/img/result/user-1.jpg" alt="Image" />
+                  </div>
+                  <span>
+                    Adam <br />
+                    Sendler
+                  </span>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <div className="result-img">
+                    <img src="assets/img/result/user-2.jpg" alt="Image" />
+                  </div>
+                  <span>
+                    James <br />
+                    Rogers
+                  </span>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <div className="result-img">
+                    <img src="assets/img/result/user-3.jpg" alt="Image" />
+                  </div>
+                  <span>
+                    Steve <br />
+                    Smith
+                  </span>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <div className="result-img">
+                    <img src="assets/img/result/user-4.jpg" alt="Image" />
+                  </div>
+                  <span>
+                    William <br />
+                    Smith
+                  </span>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <div className="result-img">
+                    <img src="assets/img/result/user-5.jpg" alt="Image" />
+                  </div>
+                  <span>
+                    James <br />
+                    Rogers
+                  </span>
+                </a>
+              </li>
             </ul>
             <a href="#" className="default-btn">
-              Buy Ticket
+              {" "}
+              Buy Ticket{" "}
             </a>
           </div>
         </div>
--- pages/results/index.js
+++ pages/results/index.js
@@ -4,8 +4,12 @@ import SearchForm from "../../components/Home/SearchForm";
 import ResultsWrapper from "../../components/Results/ResultsWrapper";
 import SingleResult from "../../components/Results/SingleResult";
 import { motion } from "framer-motion";
+const https = require("https");
 
 const Home = ({ shows }) => {
+  // console.log(shows);
+  // shows.success.map((shoe) => console.log(shoe));
+  console.log(shows.success);
   const title = "";
   return (
     <motion.div initial="initial" animate="animate" exit={{ opacity: 0 }}>
@@ -13,7 +17,7 @@ const Home = ({ shows }) => {
         <Banner title={title} />
         <SearchForm />
         <ResultsWrapper>
-          {shows.map((show) => (
+          {shows.success.map((show) => (
             <SingleResult show={show} key={show.id} />
           ))}
         </ResultsWrapper>
@@ -24,9 +28,24 @@ const Home = ({ shows }) => {
 
 export async function getStaticProps() {
   //Fetch for shows
+  // const res = await fetch(
+  //   "https://doingtimeapp.com/api/v1/show/getupcomingshows"
+  // );
+  const httpsAgent = new https.Agent({
+    rejectUnauthorized: false,
+  });
+
   const res = await fetch(
-    "https://my-json-server.typicode.com/daviemola/fakecomedyjson/upcoming_shows"
+    `https://doingtimeapp.com/api/v1/show/getupcomingshows`,
+    {
+      method: "GET",
+      headers: {
+        doingtimeid: "dYQdSFt23XRnLJLFFwkYN66JGQw5xWhXg7Rbksdg",
+      },
+      agent: httpsAgent,
+    }
   );
+
   const shows = await res.json();
 
   //return props
